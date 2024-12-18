682c4d607e7938334fdc204deab87d031e3632bb
Osuuunaaa <carlos.osuna@alumnos.upm.es>
2024-12-18 11:35:42 +0100

    Decoder mejorado. X e Y

--- Trabajo-FPGA/Trabajo-FPGA.srcs/sources_1/new/decoder.vhd
+++ Trabajo-FPGA/Trabajo-FPGA.srcs/sources_1/new/decoder.vhd
@@ -26,12 +26,12 @@ begin
             when "0111" => seg_out <= "1111000"; -- 7
             when "1000" => seg_out <= "0000000"; -- 8
             when "1001" => seg_out <= "0010000"; -- 9
-            when "1010" => seg_out <= "0001000"; -- A
-            when "1011" => seg_out <= "0000011"; -- b
-            when "1100" => seg_out <= "1000110"; -- C
-            when "1101" => seg_out <= "0100001"; -- d
-            when "1110" => seg_out <= "0000110"; -- E
-            when "1111" => seg_out <= "0001110"; -- F
+            when "1010" => seg_out <= "0001001"; -- X
+            when "1011" => seg_out <= "0010001"; -- Y
+            when "1100" => seg_out <= "1111111"; -- C
+            when "1101" => seg_out <= "1111111"; -- d
+            when "1110" => seg_out <= "1111111"; -- E
+            when "1111" => seg_out <= "1111111"; -- F
             when others => seg_out <= "1111111"; -- Apagar
         end case;
     end process;
